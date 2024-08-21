9a369ea91ac7edac179dd39913030ce8e1d960b4
Jasmine <jasminemendes@alu.ufc.br>
2024-08-21 11:31:54 -0300

    Alterações realizadas mediante feedbacks do professor + adição de algumas pages

--- app/src/main/java/com/example/placarfutsal/HistoricoActivity.kt
+++ app/src/main/java/com/example/placarfutsal/HistoricoActivity.kt
@@ -1,20 +1,31 @@
 package com.example.placarfutsal
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.placarfutsal.databinding.ActivityHistoricoBinding
+import com.example.placarfutsal.databinding.ActivityMainBinding
 
 class HistoricoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHistoricoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_historico)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityHistoricoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnVoltar.setOnClickListener{ voltar()
         }
+    }
+
+    private fun voltar(){
+        //Voltar p tela inicial
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
